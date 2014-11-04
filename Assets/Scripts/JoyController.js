c503ccfa4e60cb361b73b0a517849694a5c597da
houzhi <houzhi@youku.com>
2014-11-04 22:19:22 +0800

    Init
    Easy game sample build by Unity 3D

---
+++ Assets/Scripts/JoyController.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+
+var joyStickPlayer:Joystick;
+
+var speed:float;
+var countText:GUIText;
+var winText:GUIText;
+var count:int;
+
+function SetCountText()
+{
+	countText.text = "Count: " + count.ToString ();
+	if (count >= 12) 
+	{
+		winText.text = "YOU WIN!";
+	}
+}
+
+function Start () 
+{
+	count = 0;
+	SetCountText();
+	winText.text = "";
+
+}
+
+function Update ()
+{
+	if(joyStickPlayer.tapCount >0 )
+	{
+		var joyPosition_x = joyStickPlayer.position.x;
+		var joyPosition_y = joyStickPlayer.position.y;
+		var movement:Vector3 = new Vector3 (joyPosition_x, 0.0f, joyPosition_y);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	} else {
+		var moveHorizontal:float = Input.GetAxis("Horizontal");
+		var moveVertical:float = Input.GetAxis("Vertical");
+		var movementAxis:Vector3 = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce (movementAxis * speed * Time.deltaTime);
+	}	
+	
+}
+
+function OnTriggerEnter(other:Collider)
+{
+	if (other.gameObject.tag == "PickUp")
+	{
+		other.gameObject.SetActive(false);
+		count = count+1;
+		SetCountText();
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	/*
+	 * function Update ()
+{
+if(joyStickPlayer.tapCount >0 )
+{
+        var joyPosition_x = joyStickPlayer.position.x;
+        var joyPosition_y = joyStickPlayer.position.y;
+         
+        if(joyPosition_y != 0 || joyPosition_x != 0)
+        {
+                hero.transform.Translate(Vector3.forward* Time.deltaTime * 5);
+                hero.transform.LookAt(Vector3(hero.transform.position.x + joyPosition_x,hero.transform.position.y,hero.transform.position.z + joyPosition_y));
+                hero.animation.Play("run");
+        }
+        else
+        {
+                hero.animation.Play("idle");
+        }
+}     
+	 * */
+
+	void FixedUpdate()
+	{
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count+1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "YOU WIN!";
+		}
+	}
+
+}
